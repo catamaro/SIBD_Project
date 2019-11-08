9d287c4435cd5fe2bb427efa410566aed6c18a06
catamaro <anapamaro98@gmail.com>
2019-11-08 21:49:31 +0000

    fix queries, update populate, fix changes

--- changes.sql
+++ changes.sql
@@ -21,9 +21,8 @@ WHERE employee_VAT IN (
 /*delete 'Jane SweetTooth' from database*/
 DELETE FROM employee WHERE employee_name = 'Jane SweetTooth' AND employee_VAT IN (SELECT VAT_doctor FROM doctor);
 
-UPDATE trainee_doctor SET supervisor = (SELECT VAT_permanent FROM permanent_doctor WHERE VAT_permanent <> (SELECT employee_VAT FROM employee WHERE employee_name = 'Jane SweetTooth'))				
-	WHERE supervisor = (SELECT employee_VAT FROM employee WHERE employee_name = 'Jane SweetTooth');
-
+UPDATE trainee_doctor SET supervisor = (SELECT VAT_permanent FROM permanent_doctor)				
+	WHERE supervisor IS NULL;
 
 /*********************************change 4*********************************/
 INSERT INTO diagnostic_code values ('0T1T147', 'periodontitis');
