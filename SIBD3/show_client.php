1c98782573ce7348894cf6096abba6cc3645ca8e
mariana <mariana.fidalgo.f>
2019-11-23 19:24:18 +0000

    updated2

--- SIBD3/show_client.php
+++ SIBD3/show_client.php
@@ -1,32 +1,40 @@
 <html>
  <body>
 <?php
-$host = "db.tecnico.ulisboa.pt";
-$user = "ist187074";
-$pass = "rxmm7788";
-$dsn = "mysql:host=$host;dbname=$user";
-try
-{
-$connection = new PDO($dsn, $user, $pass);
-}
-catch(PDOException $exception)
-{
-echo("<p>Error: ");
-echo($exception->getMessage());
-echo("</p>");
-exit();
-}
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "proj_part3";
+ $db = "proj_part2";
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+   
 $client_VAT = $_REQUEST['client_vat'];
 $client_name = $_REQUEST['client_name'];
 $client_street = $_REQUEST['client_address'];
-$sql = "SELECT * FROM client";
-echo("<p>$sql</p>");
-$nrows = $connection->query($sql);
-foreach ($nrows as $row){
-	print $row['client_name'];
-	print "\n";
+$date_timestamp = $_REQUEST['date_appointment'];
+$csql = "SELECT *
+		FROM client as c
+		WHERE client_vat LIKE '%$client_VAT%'
+		AND c.client_name LIKE '%$client_name%'
+		AND c.client_street LIKE '%$client_street%'";
+echo("<h2>Clients found:</h2>");
+$crows = $conn->query($csql);
+echo("<p>$csql</p>");
+foreach ($crows as $row){
+	echo $row['client_name'] ."<br>\n";
 }
- $connection = null;
+
+$dsql = "SELECT DISTINCT employee_name
+		FROM employee as e INNER JOIN appointment as a
+		ON e.employee_VAT = a.VAT_doctor";
+
+echo("<h2>Doctor found:</h2>");
+echo("<p>$dsql</p>");
+$drows = $conn->query($dsql);
+
+foreach ((array)$drows as $row){
+	echo $row['employee_name'] ."<br>\n";
+}
+ $conn = null;
 ?>
  </body>
 </html>
