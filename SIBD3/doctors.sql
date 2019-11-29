8eb6cef25b3e6199bce8e8cee8447c3139afb976
mariana <mariana.fidalgo.f>
2019-11-29 15:22:54 +0000

    removed not used doc

--- SIBD3/doctors.sql
+++
@@ -1,7 +0,0 @@
-
-	SELECT DISTINCT employee_name
-	FROM employee AS e INNER JOIN appointment AS a
-	ON e.employee_VAT = a.VAT_doctor
-	WHERE a.date_timestamp != '2019-11-04'
-
-Select * from appointment
